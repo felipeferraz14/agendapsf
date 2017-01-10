cfa9b3ecfa7f4d4b2f971e91d16ebbe9a42df450
felipeferraz14 <felipe_ferraz14@hotmail.com>
2017-01-10 19:26:56 -0300

    Alterações

--- src/main/java/br/com/agendapsf/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/agendapsf/conf/JPAProductionConfiguration.java
@@ -24,7 +24,6 @@ public class JPAProductionConfiguration {
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		 
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-		System.out.println(dbUri);
 
 	    String username = dbUri.getUserInfo().split(":")[0];
 	    String password = dbUri.getUserInfo().split(":")[1];
